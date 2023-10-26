e1c74cb60702bcb58e1c76f89dbac8028c129476
Matheus Alves <matalves12345@gmail.com>
2023-10-26 12:35:04 -0300

    teste ga

--- bubbles_2.0.py
+++ bubbles_2.0.py
@@ -5,21 +5,39 @@ from objs.grid_file import *
 from objs.shooter_file import *
 from objs.game_objects import *
 import pygame as pg
+import neat
+import os
+
 pg.init()
 
+generation = 0
 
-def main():
+ia_play = True
+
+def main(genomes, config):
+	global generation
+	generation += 1
+
+	networks = []
+	genomes_list = []
+	guns = []
+	
+	for _, genome in genomes:
+		gun = Shooter(pos = BOTTOM_CENTER)
+		gun.putInBox()
+		
+		
+		
+		network = neat.nn.FeedForwardNetwork.create(genome, config)
+		networks.append(network)
+		genome.fitness = 0
+		genomes_list.append(genome)
+		guns.append(gun)
 
 	# Create background
 	background = Background()
-
-	# Initialize gun, position at bottom center of the screen
-	gun = Shooter(pos = BOTTOM_CENTER)
-	gun.putInBox()	
-
-	grid_manager = GridManager()
+	
 	game = Game()	
-	cheat_manager = CheatManager(grid_manager, gun)
 
 	# Starting mouse position
 	mouse_pos = (DISP_W/2, DISP_H/2)
@@ -28,52 +46,65 @@ def main():
 	while not game.over:		
 
 		# quit when you press the x
-		for event in pg.event.get():
-			if event.type == pg.QUIT:
-				pg.quit()
-				quit()
-
-			# get mouse position
-			#if event.type == pg.MOUSEMOTION: mouse_pos = pg.mouse.get_pos()
-				
-			# if you click, fire a bullet
-			#if event.type == pg.MOUSEBUTTONDOWN: gun.fire()
-			
-			if event.type == pg.KEYDOWN:
-				cheat_manager.view(event) # if a key is pressed, the cheat manager should know about it
-
-				# Ctrl+C to quit
-				if event.key == pg.K_SPACE:
-					gun.fire()
-				if event.key == pg.K_LEFT:
-					mouse_pos = (mouse_pos[0] - 10 , mouse_pos[1])
-				if event.key == pg.K_RIGHT:
-					mouse_pos = (mouse_pos[0] + 10 , mouse_pos[1])
-				if event.key == pg.K_c and pg.key.get_mods() & pg.KMOD_CTRL:
+		if not ia_play:
+			for event in pg.event.get():
+				if event.type == pg.QUIT:
 					pg.quit()
 					quit()
 
-		
-		background.draw()				# Draw BG first		
+				# get mouse position
+				#if event.type == pg.MOUSEMOTION: mouse_pos = pg.mouse.get_pos()
+					
+				# if you click, fire a bullet
+				#if event.type == pg.MOUSEBUTTONDOWN: gun.fire()
+				
+				if event.type == pg.KEYDOWN:
+					cheat_manager.view(event) # if a key is pressed, the cheat manager should know about it
 
-		grid_manager.view(gun, game)	# Check collision with bullet and update grid as needed		
+					# Ctrl+C to quit
+					if event.key == pg.K_SPACE:
+						gun.fire()
+					if event.key == pg.K_LEFT:
+						mouse_pos = (mouse_pos[0] - 10 , mouse_pos[1])
+					if event.key == pg.K_RIGHT:
+						mouse_pos = (mouse_pos[0] + 10 , mouse_pos[1])
+					if event.key == pg.K_c and pg.key.get_mods() & pg.KMOD_CTRL:
+						pg.quit()
+						quit()
+		else:
+			for gun in guns:
 
-		gun.rotate(mouse_pos)			# Rotate the gun if the mouse is moved		
-		gun.draw_bullets()				# Draw and update bullet and reloads	
+				background.draw()				# Draw BG first		
 
-		game.drawScore()				# draw score
+				grid_manager.view(gun, game)	# Check collision with bullet and update grid as needed		
 
-		pg.display.update()		
-		clock.tick(60)					# 60 FPS
+				gun.rotate(mouse_pos)			# Rotate the gun if the mouse is moved		
+				gun.draw_bullets()				# Draw and update bullet and reloads	
 
-	game.gameOverScreen(grid_manager, background)
+				game.drawScore()				# draw score
+
+				pg.display.update()		
+				clock.tick(60)					# 60 FPS
+
+			game.gameOverScreen(grid_manager, background)
 
 	return
 
-def run():
-	pass
+def run(path_config):
+	config = neat.config.Config(neat.DefaultGenome,
+								neat.DefaultReproduction,
+								neat.DefaultSpeciesSet,
+								neat.DefaultStagnation,
+								path_config)
+
+	population = neat.Population(config)
+	population.add_reporter(neat.StdOutReporter(True))
+	population.add_reporter(neat.StatisticsReporter())
+	population.run(main, 50)
 
 if __name__ == '__main__': 
-	while True: main()
+	path = os.path.dirname(__file__)
+	path_config = os.path.join(path, 'config.txt')
+	run(path_config)
 
 
